d7b8198e4bb3624f7451c5fb75137da5166fbab0
Rémy HUBSCHER <rhubscher@mozilla.com>
2014-11-27 14:51:39 +0100

    Add missing files

---
+++ html5/src/js/components/FileDropZone.js
@@ -0,0 +1,15 @@
+/** @jsx React.DOM */
+
+"use strict";
+
+var React = require("react");
+
+var FileDropZone = React.createClass({
+  render: function() {
+    return (<div>
+      {"FileDropZone"}
+    </div>);
+  }
+});
+
+module.exports = FileDropZone;
---
+++ html5/src/js/components/FilesList.js
@@ -0,0 +1,15 @@
+/** @jsx React.DOM */
+
+"use strict";
+
+var React = require("react");
+
+var FilesList = React.createClass({
+  render: function() {
+    return (<div>
+      {"FilesList"}
+    </div>);
+  }
+});
+
+module.exports = FilesList;
